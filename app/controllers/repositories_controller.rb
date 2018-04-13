94cccbd34d737432bbb9f1f339caebeb2ee78ea9
David Towster <djt4xe@virginia.edu>
2018-04-12 18:07:50 -0700

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,12 @@
 class RepositoriesController < ApplicationController
   def index
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    @repos_array = JSON.parse(response.body)
+    github = GithubRepo.new({name: "", html_url: ""})
+    @repos_array = github.repositories(session[:token])
   end
 
   def create
-    response = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
+    github = GithubRepo.new({name: "", html_url: ""})
+    github.create_repository(session[:token], params[:name])
     redirect_to '/'
   end
 end
--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -2,6 +2,11 @@ class GithubRepo
 
   attr_reader :name, :url
 
+  def authenticate_user(client_id)
+    url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=repo"
+    url
+  end
+
   def authenticate(client_id, github_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: github_secret, code: code}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
@@ -13,6 +18,15 @@ class GithubRepo
 
     user_json = JSON.parse(user_response.body)
     user_json["login"]
+  end
+
+  def repositories(session_token)
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session_token}", 'Accept' => 'application/json'}
+    JSON.parse(response.body)
+  end
+
+  def create_repository(session_token, repo_name)
+    response = Faraday.post "https://api.github.com/user/repos", {name: repo_name}.to_json, {'Authorization' => "token #{session_token}", 'Accept' => 'application/json'}
   end
 
   def initialize(hash)
